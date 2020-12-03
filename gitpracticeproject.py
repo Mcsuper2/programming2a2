36c31aa066399cc8ccc40cdb9575aa5a3575d1a1
Marcus Yu <marcusyu478@gmail.com>
2020-12-03 10:05:05 -0800

    Implemented fib algorithm with n == 1 and 2.

--- gitpracticeproject.py
+++ gitpracticeproject.py
@@ -5,6 +5,10 @@
 def main():
     # find the nth Fibonacci number
     # e.g. 1, 2, 3, 5, 8, 13, 21...
+    n = int(input("What fibonacci number do you want to find?"))
+
+    if n in [1, 2]:
+        print("The nth fib number is 1")
 
 
 if __name__ == "__main__":
