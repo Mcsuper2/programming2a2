4a56c9fd82bc548bed2a1c466c65025bf4edd040
Marcus Yu <marcusyu478@gmail.com>
2020-12-03 09:58:48 -0800

    Commit Git Practice Project

---
+++ gitpracticeproject.py
@@ -0,0 +1,12 @@
+# GHPracticeProject.py
+
+# Practice project to demonstrate Git workflow
+
+def main():
+    # find the nth Fibonacci number
+    # e.g. 1, 2, 3, 5, 8, 13, 21...
+
+
+if __name__ == "__main__":
+    main()
+
